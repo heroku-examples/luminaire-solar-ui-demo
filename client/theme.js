746ea4167fcc03f65c19d0ea82d52604800586ce
Heroku DevRel <herokudevrel@gmail.com>
2024-09-03 16:15:47 -0400

    Update theme.js title and color

--- client/theme.js
+++ client/theme.js
@@ -1,9 +1,9 @@
 import { createTheme } from '@mantine/core';
 
-export const title = 'Lumina Solar';
+export const title = 'Lumina Solar & Wind';
 export const year = '2024';
 
 export const theme = createTheme({
   fontFamily: 'Inter, sans-serif',
-  primaryColor: 'violet',
+  primaryColor: 'red',
 });

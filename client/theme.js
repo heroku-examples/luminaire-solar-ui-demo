4cccb7482bb06eb40dca7c4750438d93eb4698e3
Heroku DevRel <herokudevrel@gmail.com>
2024-10-04 14:13:26 +0100

    Update theme.js

--- client/theme.js
+++ client/theme.js
@@ -1,6 +1,6 @@
 import { createTheme } from '@mantine/core';
 
-export const title = 'Lumina Solar';
+export const title = 'Lumina Solar Inc.';
 export const year = '2024';
 
 export const theme = createTheme({

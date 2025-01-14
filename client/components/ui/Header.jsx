e11b0749e349e9ec4fc0f5d89005cd8183720c2c
Aschente0 <carlosyukming.lam@hotmail.com>
2025-01-14 14:43:49 -0500

    fixes to header, paperclip icon

--- client/components/ui/Header.jsx
+++ client/components/ui/Header.jsx
@@ -50,8 +50,8 @@ export function Header() {
   });
 
   return (
-    <header className={classes.header}>
-      <div className={classes.inner}>
+    <header className="z-20 w-full h-16 bg-white fixed top-0">
+      <div className="px-16 py-4 flex justify-between items-center">
         <Link to="/demo" className="nav-link">
           <Group>
             <FullLogo />

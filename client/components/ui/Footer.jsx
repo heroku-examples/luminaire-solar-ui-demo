58687adfd375e85922a510104ef5a77934aa2aef
Aschente0 <carlosyukming.lam@hotmail.com>
2025-02-26 15:11:15 -0500

    homepage ui fixes, desktop footer ui

--- client/components/ui/Footer.jsx
+++ client/components/ui/Footer.jsx
@@ -1,10 +1,68 @@
-import { Container, Text, Group, Grid } from '@mantine/core';
-import { title, year } from '@/theme.js';
+import { Link } from 'react-router-dom';
+import FullLogo from '../icons/Logo';
 
 export function Footer() {
+  const customerServiceLinks = [
+    {
+      link: '/contact',
+      label: 'Contact Us',
+    },
+    { link: '/order-status', label: 'Order Status' },
+    { link: '/faq', label: 'FAQ' },
+  ];
+
+  const aboutUsLinks = [
+    {
+      link: '/about',
+      label: 'Our Story',
+    },
+    { link: '/press', label: 'Press' },
+    { link: '/blog', label: 'Blog' },
+  ];
   return (
     <footer>
-      <Container mt="lg">
+      <div className="p-12 pb-36 bg-light-grey flex justify-between">
+        <div>
+          <Link to="/" className="nav-link" style={{ padding: 0 }}>
+            <FullLogo />
+          </Link>
+        </div>
+        <div className="flex flex-row gap-12">
+          <div className="flex flex-col gap-y-2">
+            <p className="min-w-48 text-sm">Customer Service</p>
+            {customerServiceLinks.map((link) => {
+              return (
+                <div key={`footer${link.link}`}>
+                  <Link
+                    to={link.link}
+                    className={`text-dark-grey font-medium text-sm`}
+                    style={{ padding: 0 }}
+                  >
+                    {link.label}
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <p className="min-w-48 text-sm">About Us</p>
+            {aboutUsLinks.map((link) => {
+              return (
+                <div key={`footer${link.link}`}>
+                  <Link
+                    to={link.link}
+                    className={`text-dark-grey font-medium text-sm`}
+                    style={{ padding: 0 }}
+                  >
+                    {link.label}
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      {/* <Container mt="lg">
         <Grid justify="center">
           <Grid.Col span={12} md={4}>
             <Text size="sm" c="dimmed" align="center">
@@ -17,7 +75,7 @@ export function Footer() {
             </Text>
           </Group>
         </Grid>
-      </Container>
+      </Container> */}
     </footer>
   );
 }

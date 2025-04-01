37c53e8b7784e5a40cab9826f3e4114fbf584c8a
Heroku DevRel <herokudevrel@gmail.com>
2025-04-01 13:08:09 +0100

    Update theme.js

--- client/theme.js
+++ client/theme.js
@@ -1,9 +1,9 @@
 import { createTheme } from '@mantine/core';
 
-export const title = 'Luminaire Solar';
+export const title = 'Sun Solar';
 export const year = '2024';
 
 export const theme = createTheme({
   fontFamily: 'Inter, sans-serif',
-  primaryColor: 'violet',
+  primaryColor: 'red',
 });

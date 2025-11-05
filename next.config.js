278de7ec8b3ac1f70e14709fb1f9d4c8a9f3fe77
Julian Duque <julianduquej@gmail.com>
2025-11-05 18:41:47 -0500

    fix: remove route rewrites

--- next.config.js
+++ next.config.js
@@ -10,14 +10,6 @@ const nextConfig = {
     ],
     unoptimized: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.API_URL || 'http://0.0.0.0:3001'}/api/:path*`,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
